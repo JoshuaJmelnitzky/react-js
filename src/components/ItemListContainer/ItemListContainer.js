6fa55688f54f1fe16b6aa7bbf46c99d7dce35b52
Joshua <joshuajmelnitzkysj@gmail.com>
2021-12-31 07:09:36 -0300

    Desafio 6

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { getFetch } from '../../helpers/mock'
+import Item from './Item'
 
 const ItemListContainer = ({greeting}) => {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+    
+        getFetch
+        .then(resp => setProducts(resp))
+        .catch(err => console.log(err))
+        .finally(() => setLoading(false))
+    
+    }, [])
+
+
     return (
+
         <div>
             <h1>{greeting}</h1>
+            { loading? <h2>Cargando ...</h2>:
+            
+            products.map (prod => <Item detalle = {prod.id, prod.brand, prod.model, prod.photo} />
+                
+            )}
         </div>
     )
 }
